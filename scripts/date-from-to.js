fd60ce1d2d62dc0ac3faf1606bc2617e1d290483
kriachko-artem <kriachkoartem@gmail.com>
2021-11-11 00:21:14 +0200

    beta-finish

--- scripts/date-from-to.js
+++ scripts/date-from-to.js
@@ -1,31 +1,10 @@
-$( function() {
-    var dateFormat = "dd/mm/yyyy",
-      from = $( "#date-from" )
-        .datepicker({
-          defaultDate: "+1w",
-          changeMonth: true,
-          numberOfMonths: 3
-        })
-        .on( "change", function() {
-          to.datepicker( "option", "minDate", getDate( this ) );
-        }),
-      to = $( "#date-to" ).datepicker({
-        defaultDate: "+1w",
-        changeMonth: true,
-        numberOfMonths: 3
-      })
-      .on( "change", function() {
-        from.datepicker( "option", "maxDate", getDate( this ) );
-      });
- 
-    function getDate( element ) {
-      var date;
-      try {
-        date = $.datepicker.parseDate( dateFormat, element.value );
-      } catch( error ) {
-        date = null;
-      }
- 
-      return date;
-    }
-  } );
+$(function() {
+  $('#date-from, #date-to').datepicker({
+      position: 'bottom left',
+      clearButton: true,
+      onSelect(formattedDate, date, inst) {
+        inst.hide();
+      },
+    });
+
+  });
